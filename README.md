04d8b91ff365489c1e3e97eb56838f25c692656a
rahul <rannareddy@newrelic.com>
2024-11-14 16:12:32 +0530

    Done till chapter 5

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # go-backend-greenlight
 Following along with Let's Go Further
+
+### cmd/api
+Contains application specific code for the Greenlight API.
+This includes code for running the server, reading and writing HTTP requests, and managing authentication.
+
+### internal
+Contains code for db interactions, data validation, sending emails etc.
+Any code which isn’t application-specific and can potentially be reused will live in here.
+
+### migrations
+Contains SQL migrations files
+
+### remote
+Contains config files and setup scripts for prod server.
+
+### Makefile
+Contains recipes for automating common CLI tasks
---
+++ cmd/api/errors.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// The logError() method is a generic helper for logging an error message. Later in the // book we'll upgrade this to use structured logging, and record additional information // about the request including the HTTP method and URL.
+func (app *application) logError(r *http.Request, err error) {
+	app.logger.Println(err)
+}
+
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{}) {
+	env := envelope{"error": message}
+	// Write the response using the writeJSON() helper. If this happens to return an // error then log it, and fall back to sending the client an empty response with a // 500 Internal Server Error status code.
+	err := app.writeJSON(w, status, env, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(500)
+	}
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+}
+
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+	message := "the server encountered a problem and could not process your request"
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the requested resource could not be found"
+	app.errorResponse(w, r, http.StatusNotFound, message)
+}
+
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
+
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
