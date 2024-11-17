7baaa287c7f1268f45bf92a856b76c6c9ff04ae4
rahul <rannareddy@newrelic.com>
2024-11-17 14:48:55 +0530

    Created all the endpoints for the movies API

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module greenlight.rahulreddy.in
 go 1.23.2
 
 require (
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.10.9
 )
---
+++ internal/data/models.go
@@ -0,0 +1,19 @@
+package data
+
+import (
+	"database/sql"
+	"errors"
+)
+
+var ErrRecordNotFound = errors.New("record not found")
+var ErrEditConflict = errors.New("edit conflict")
+
+type Models struct {
+	Movies MovieModel
+}
+
+func NewModels(db *sql.DB) Models {
+	return Models{
+		Movies: MovieModel{DB: db},
+	}
+}
